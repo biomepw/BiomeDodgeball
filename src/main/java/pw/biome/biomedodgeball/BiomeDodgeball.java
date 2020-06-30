a6c5f1d31aebb04499f0ec8243e1aff8eef651c6
Jordyn <jordyn@husk.pro>
2020-06-30 11:10:36 +0800

    begin dodgeball

---
+++ src/main/java/pw/biome/biomedodgeball/BiomeDodgeball.java
@@ -0,0 +1,16 @@
+package pw.biome.biomedodgeball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BiomeDodgeball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
